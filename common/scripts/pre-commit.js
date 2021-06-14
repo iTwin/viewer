a36726dea0a47683979123cbccfc330c93ee8c84
Arun George <aruniverse@users.noreply.github.com>
2021-06-14 14:52:48 -0400

    bump viewer repo to use cra 4.0

--- common/scripts/pre-commit.js
+++ common/scripts/pre-commit.js
@@ -5,9 +5,9 @@ async function preCommit() {
     config: {
       "*.{ts,tsx}": [
         "node ./common/scripts/copyright-linter.js --",
-        "node --max_old_space_size=4096 ./common/scripts/node_modules/eslint/bin/eslint.js --config ./common/scripts/.eslintrc.ts.autofix.json --ignore-path ./.eslintignore --fix",
+        "node --max_old_space_size=4096 ./common/scripts/node_modules/eslint/bin/eslint.js --config ./common/scripts/.eslintrc.ts.autofix.json --ignore-path ./.eslintignore --resolve-plugins-relative-to ./common/scripts --fix",
         "node ./common/scripts/node_modules/prettier --write --config ./.prettierrc --ignore-path ./.prettierignore",
-        "node --max_old_space_size=4096 ./common/scripts/node_modules/eslint/bin/eslint.js --config ./common/scripts/.eslintrc.ts.json --ignore-path ./.eslintignore --color",
+        "node --max_old_space_size=4096 ./common/scripts/node_modules/eslint/bin/eslint.js --config ./common/scripts/.eslintrc.ts.json --ignore-path ./.eslintignore --color --resolve-plugins-relative-to ./common/scripts",
       ],
       "*.{md,json}": [
         "node ./common/scripts/node_modules/prettier --write --config ./.prettierrc --ignore-path ./.prettierignore",
