749a5143c4eb4c722f6526a6de84650dd5ea9e09
kckst8 <6283674+kckst8@users.noreply.github.com>
2021-06-23 12:35:21 -0400

    remove unused backend initialization code; add en-PSEUDO strings

--- packages/modules/desktop-viewer-react/src/services/Initializer.ts
+++ packages/modules/desktop-viewer-react/src/services/Initializer.ts
@@ -3,65 +3,14 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
-import { ClientRequestContext } from "@bentley/bentleyjs-core";
 import {
   ElectronApp,
   ElectronAppOpts,
 } from "@bentley/electron-manager/lib/ElectronFrontend";
-import { BentleyCloudRpcParams } from "@bentley/imodeljs-common";
 import { IModelApp } from "@bentley/imodeljs-frontend";
-import { UrlDiscoveryClient } from "@bentley/itwin-client";
-import { getIModelAppOptions, IModelBackendOptions } from "@itwin/viewer-react";
+import { getIModelAppOptions } from "@itwin/viewer-react";
 
 import { DesktopViewerProps } from "../types";
-
-const getHostedConnectionInfo = async (
-  backendOptions?: IModelBackendOptions
-): Promise<BentleyCloudRpcParams> => {
-  const urlClient = new UrlDiscoveryClient();
-  const requestContext = new ClientRequestContext();
-
-  const orchestratorUrl = await urlClient.discoverUrl(
-    requestContext,
-    `iModelJsOrchestrator.K8S`,
-    backendOptions?.buddiRegion
-  );
-
-  if (backendOptions?.hostedBackend) {
-    if (!backendOptions.hostedBackend.title) {
-      //TODO localize
-      throw new Error("Please provide the title for the iTwin.js backend");
-    }
-    if (!backendOptions.hostedBackend.version) {
-      //TODO localize
-      throw new Error("Please provide a version for the iTwin.js backend");
-    }
-    return {
-      info: {
-        title: backendOptions.hostedBackend.title,
-        version: backendOptions.hostedBackend.version,
-      },
-      uriPrefix: orchestratorUrl,
-    };
-  } else {
-    return {
-      info: { title: "general-purpose-imodeljs-backend", version: "v2.0" },
-      uriPrefix: orchestratorUrl,
-    };
-  }
-};
-
-const initializeRpcParams = async (
-  backendOptions?: IModelBackendOptions
-): Promise<BentleyCloudRpcParams> => {
-  // if rpc params for a custom backend are provided, use those
-  if (backendOptions?.customBackend && backendOptions.customBackend.rpcParams) {
-    return backendOptions.customBackend.rpcParams;
-  } else {
-    // otherwise construct params for a hosted connection
-    return await getHostedConnectionInfo(backendOptions);
-  }
-};
 
 export class DesktopInitializer {
   private static _initialized: Promise<void>;
@@ -93,7 +42,6 @@ export class DesktopInitializer {
       this._initialized = new Promise(async (resolve, reject) => {
         try {
           this._reject = () => reject("Desktop Startup Cancelled");
-          const rpcParams = await initializeRpcParams(options?.backend);
           const electronViewerOpts: ElectronAppOpts = {
             iModelApp: getIModelAppOptions(options),
           };
