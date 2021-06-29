23f5cda24146799813a02f62c612820dfee0c6f3
kckst8 <6283674+kckst8@users.noreply.github.com>
2021-06-29 14:45:00 -0400

    Revert "Bump rush pnpm (#12)"
    This reverts commit 5cb62f01a48783d4b9161c9454000324a24d7aa8.

--- packages/apps/desktop-viewer-test/src/backend/AppInfo.ts
+++ packages/apps/desktop-viewer-test/src/backend/AppInfo.ts
@@ -2,7 +2,6 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-
 export const appInfo = {
   id: "itwin-viewer",
   title: "iTwin Desktop Viewer",
