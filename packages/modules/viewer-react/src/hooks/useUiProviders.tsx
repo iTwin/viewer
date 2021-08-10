68cd82908f5cc5ed53131140ea5a69530347aaaf
spap975 <48142512+spap975@users.noreply.github.com>
2021-08-10 12:36:07 -0400

    Updated package.json file and made small changes

--- packages/modules/viewer-react/src/hooks/useUiProviders.tsx
+++ packages/modules/viewer-react/src/hooks/useUiProviders.tsx
@@ -2,28 +2,35 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-import { TreeWidgetUiItemsProvider } from "@bentley/tree-widget-react/lib/components/TreeWidgetUiItemsProvider";
+import { TreeWidgetUiItemsProvider } from "@bentley/tree-widget-react";
 import { UiItemsManager, UiItemsProvider } from "@bentley/ui-abstract";
 import { useEffect } from "react";
 
-export function useUiProviders(uiProviders?: UiItemsProvider[]): void {
-  useEffect(() => {
-    const defaultProviders: UiItemsProvider[] = [
-      new TreeWidgetUiItemsProvider(),
-    ];
+import { ItwinViewerUi } from "..";
 
-    const _uiProviders = uiProviders
-      ? uiProviders.concat(defaultProviders)
+export function useUiProviders(
+  customUiProviders?: UiItemsProvider[],
+  defaultUiConfig?: ItwinViewerUi
+): void {
+  useEffect(() => {
+    const defaultProviders: UiItemsProvider[] = [];
+
+    if (defaultUiConfig?.hideTreeView !== false) {
+      defaultProviders.push(new TreeWidgetUiItemsProvider());
+    }
+
+    const uiProviders = customUiProviders
+      ? customUiProviders.concat(defaultProviders)
       : defaultProviders;
 
-    _uiProviders.forEach((uiProvider) => {
+    uiProviders.forEach((uiProvider) => {
       UiItemsManager.register(uiProvider);
     });
 
     return () => {
-      _uiProviders.forEach((uiProvider) => {
+      uiProviders.forEach((uiProvider) => {
         UiItemsManager.unregister(uiProvider.id);
       });
     };
-  }, [uiProviders]);
+  }, [customUiProviders]);
 }
