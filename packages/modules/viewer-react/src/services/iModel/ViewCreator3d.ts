0714657970af5534c16ae0720c2f6e71e9ea60b0
Stefan Retief <stefan.retief@bentley.com>
2021-09-14 13:33:57 -0700

    Inherit viewcreator provided from frontend package

--- packages/modules/viewer-react/src/services/iModel/ViewCreator3d.ts
+++ packages/modules/viewer-react/src/services/iModel/ViewCreator3d.ts
@@ -1,9 +1,8 @@
 /*---------------------------------------------------------------------------------------------
- * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
- * See LICENSE.md in the project root for license terms and full copyright notice.
- *--------------------------------------------------------------------------------------------*/
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
 
-// TODO: This is a, EDITED clone of the iTwin.js class. Once the issue with the models query is resolved in core, this should be deprecated. However, there is additional viewport customization logic that would need to be accounted for in core or elsewhere in this package
 
 /*
 API for creating a 3D default view for an iModel.
@@ -12,34 +11,14 @@ Either takes in a list of modelIds, or displays all 3D models by default.
 
 import {
   Id64,
-  Id64Array,
-  Id64String,
-  IModelStatus,
 } from "@bentley/bentleyjs-core";
-import { Range3d } from "@bentley/geometry-core";
 import {
-  Camera,
-  CategorySelectorProps,
-  Code,
-  DisplayStyle3dProps,
-  IModel,
-  IModelError,
-  IModelReadRpcInterface,
-  ModelSelectorProps,
-  RenderMode,
-  ViewDefinition3dProps,
-  ViewQueryParams,
-  ViewStateProps,
-} from "@bentley/imodeljs-common";
-import {
-  Environment,
   FitViewTool,
   IModelApp,
-  IModelConnection,
   ScreenViewport,
-  SpatialViewState,
   StandardViewId,
-  ViewState,
+  ViewCreator3d as ViewCreator,
+  ViewState
 } from "@bentley/imodeljs-frontend";
 
 import { ViewCreator3dOptions } from "../../types";
@@ -53,12 +32,7 @@ import { ViewCreator3dOptions } from "../../types";
  * ```
  * @public
  */
-export class ViewCreator3d {
-  /**
-   * Constructs a ViewCreator3d using an [[IModelConnection]].
-   * @param _imodel [[IModelConnection]] to query for categories and/or models.
-   */
-  constructor(private _imodel: IModelConnection) {}
+export class ViewCreator3d extends ViewCreator {
 
   /**
    * Creates a default [[ViewState3d]] based on the model ids passed in. If no model ids are passed in, all 3D models in the iModel are used.
@@ -70,24 +44,7 @@ export class ViewCreator3d {
     options?: ViewCreator3dOptions,
     modelIds?: string[]
   ): Promise<ViewState> {
-    const models = modelIds ? modelIds : await this._getAllModels();
-    if (models === undefined || models.length === 0) {
-      throw new IModelError(
-        IModelStatus.BadModel,
-        "ViewCreator3d.createDefaultView: no 3D models found in iModel"
-      );
-    }
-
-    const defaultViewId = await this._imodel.views.queryDefaultViewId();
-    const props = await this._createViewStateProps(
-      models,
-      defaultViewId,
-      options
-    );
-    const viewState = SpatialViewState.createFromProps(props, this._imodel);
-    try {
-      await viewState.load();
-    } catch {}
+    const viewState = await super.createDefaultView(options, modelIds);
 
     // configure the view
     IModelApp.viewManager.onViewOpen.addOnce((viewPort: ScreenViewport) => {
@@ -98,7 +55,7 @@ export class ViewCreator3d {
       }
 
       // failing that, if there is a valid default view id, adjust the volume but otherwise retain the view as is
-      if (Id64.isValidId64(defaultViewId)) {
+      if (Id64.isValidId64(viewState.id)) {
         if (options?.standardViewId) {
           viewState.setStandardRotation(options.standardViewId);
         }
@@ -136,233 +93,4 @@ export class ViewCreator3d {
 
     return viewState;
   }
-
-  /**
-   * Generates a view state props object for creating a view. Merges display styles with a seed view if the options.useSeedView is true
-   * @param models Models to put in view props
-   * @param options view creation options like camera On and skybox On
-   */
-  private async _createViewStateProps(
-    models: Id64String[],
-    defaultViewId: string,
-    options?: ViewCreator3dOptions
-  ): Promise<ViewStateProps> {
-    // Use dictionary model in all props
-    const dictionaryId = IModel.dictionaryId;
-    const categories: Id64Array = await this._getAllCategories();
-
-    // model extents
-    const modelProps = await this._imodel.models.queryModelRanges(models);
-    const modelExtents = Range3d.fromJSON(modelProps[0]);
-    let originX = modelExtents.low.x;
-    let originY = modelExtents.low.y;
-    const originZ = modelExtents.low.z;
-    let deltaX = modelExtents.xLength();
-    let deltaY = modelExtents.yLength();
-    const deltaZ = modelExtents.zLength();
-
-    // if vp aspect given, update model extents to fit view
-    if (options?.vpAspect) {
-      const modelAspect = deltaY / deltaX;
-
-      if (modelAspect > options.vpAspect) {
-        const xFix = deltaY / options.vpAspect;
-        originX = originX - xFix / 2;
-        deltaX = deltaX + xFix;
-      } else if (modelAspect < options.vpAspect) {
-        const yFix = deltaX * options.vpAspect;
-        originY = originY - yFix / 2;
-        deltaY = deltaY + yFix;
-      }
-    }
-
-    const categorySelectorProps: CategorySelectorProps = {
-      categories,
-      code: Code.createEmpty(),
-      model: dictionaryId,
-      classFullName: "BisCore:CategorySelector",
-    };
-
-    const modelSelectorProps: ModelSelectorProps = {
-      models,
-      code: Code.createEmpty(),
-      model: dictionaryId,
-      classFullName: "BisCore:ModelSelector",
-    };
-
-    const cameraData = new Camera();
-    const cameraOn = options?.cameraOn ? options.cameraOn : false;
-    const viewDefinitionProps: ViewDefinition3dProps = {
-      categorySelectorId: "",
-      displayStyleId: "",
-      code: Code.createEmpty(),
-      model: dictionaryId,
-      origin: { x: originX, y: originY, z: originZ },
-      extents: { x: deltaX, y: deltaY, z: deltaZ },
-      classFullName: "BisCore:SpatialViewDefinition",
-      cameraOn,
-      camera: {
-        lens: cameraData.lens.toJSON(),
-        focusDist: cameraData.focusDist,
-        eye: cameraData.eye.toJSON(),
-      },
-    };
-
-    const displayStyleProps: DisplayStyle3dProps = {
-      code: Code.createEmpty(),
-      model: dictionaryId,
-      classFullName: "BisCore:DisplayStyle",
-      jsonProperties: {
-        styles: {
-          viewflags: {
-            renderMode: RenderMode.SmoothShade,
-            noSourceLights: false,
-            noCameraLights: false,
-            noSolarLight: false,
-            noConstruct: true,
-            noTransp: false,
-            visEdges: false,
-            backgroundMap: this._imodel.isGeoLocated,
-          },
-          environment:
-            options !== undefined &&
-            options.skyboxOn !== undefined &&
-            options.skyboxOn
-              ? new Environment({ sky: { display: true } }).toJSON()
-              : undefined,
-        },
-      },
-    };
-
-    const viewStateProps: ViewStateProps = {
-      displayStyleProps,
-      categorySelectorProps,
-      modelSelectorProps,
-      viewDefinitionProps,
-    };
-
-    // merge seed view props if needed
-    return options?.useSeedView
-      ? this._mergeSeedView(viewStateProps, defaultViewId)
-      : viewStateProps;
-  }
-
-  /**
-   * Merges a seed view in the iModel with the passed view state props. It will be a no-op if there are no default 3D views in the iModel
-   * @param viewStateProps Input view props to be merged
-   */
-  private async _mergeSeedView(
-    viewStateProps: ViewStateProps,
-    defaultViewId: string
-  ): Promise<ViewStateProps> {
-    const viewId = await this._getSeedViewId(defaultViewId);
-    // Handle iModels without any default view id
-    if (viewId === undefined) {
-      return viewStateProps;
-    }
-
-    const seedViewState = (await this._imodel.views.load(
-      viewId
-    )) as SpatialViewState;
-    const seedViewStateProps = {
-      categorySelectorProps: seedViewState.categorySelector.toJSON(),
-      modelSelectorProps: seedViewState.modelSelector.toJSON(),
-      viewDefinitionProps: seedViewState.toJSON(),
-      displayStyleProps: seedViewState.displayStyle.toJSON(),
-    };
-    const mergedDisplayProps = seedViewStateProps.displayStyleProps;
-    if (mergedDisplayProps.jsonProperties !== undefined) {
-      mergedDisplayProps.jsonProperties.styles = {
-        ...mergedDisplayProps.jsonProperties.styles,
-        ...viewStateProps.displayStyleProps.jsonProperties!.styles,
-      };
-    }
-
-    return {
-      ...seedViewStateProps,
-      ...viewStateProps,
-      displayStyleProps: mergedDisplayProps,
-    };
-  }
-
-  /**
-   * Get ID of default view.
-   */
-  private async _getSeedViewId(
-    defaultViewId: string
-  ): Promise<Id64String | undefined> {
-    const viewId = defaultViewId;
-    const params: ViewQueryParams = {};
-    params.from = SpatialViewState.classFullName;
-    params.where = `ECInstanceId=${viewId}`;
-
-    // Check validity of default view
-    const viewProps =
-      await IModelReadRpcInterface.getClient().queryElementProps(
-        this._imodel.getRpcProps(),
-        params
-      );
-    if (viewProps.length === 0) {
-      // Return the first view we can find
-      const viewList = await this._imodel.views.getViewList({
-        wantPrivate: false,
-      });
-      if (viewList.length === 0) {
-        return undefined;
-      }
-
-      const spatialViewList = viewList.filter(
-        (value: IModelConnection.ViewSpec) =>
-          value.class.indexOf("Spatial") !== -1
-      );
-      if (spatialViewList.length === 0) {
-        return undefined;
-      }
-
-      return spatialViewList[0].id;
-    }
-
-    return viewId;
-  }
-
-  /**
-   * Get all categories containing elements
-   */
-  private async _getAllCategories(): Promise<Id64Array> {
-    // Only use categories with elements in them
-    const query = `SELECT DISTINCT Category.Id AS id FROM BisCore.GeometricElement3d WHERE Category.Id IN (SELECT ECInstanceId FROM BisCore.SpatialCategory)`;
-    const categories: Id64Array = await this._executeQuery(query);
-
-    return categories;
-  }
-
-  /**
-   * Get all PhysicalModel ids in the connection
-   */
-  private async _getAllModels(): Promise<Id64Array> {
-    let query =
-      "SELECT ECInstanceId FROM Bis.GeometricModel3D WHERE IsPrivate = false AND IsTemplate = false AND (isNotSpatiallyLocated = false OR isNotSpatiallyLocated IS NULL)";
-    let models = [];
-    try {
-      models = await this._executeQuery(query);
-    } catch {
-      // possible that the isNotSpatiallyLocated property is not available in the iModel's schema
-      query =
-        "SELECT ECInstanceId FROM Bis.GeometricModel3D WHERE IsPrivate = false AND IsTemplate = false";
-      models = await this._executeQuery(query);
-    }
-
-    return models;
-  }
-
-  /**
-   * Helper function to execute ECSql queries.
-   */
-  private _executeQuery = async (query: string) => {
-    const rows = [];
-    for await (const row of this._imodel.query(query)) {
-      rows.push(row.id);
-    }
-    return rows;
-  };
 }
