9651e90b32c97e5660a1f88cfc67138e96843f2c
Stefan Retief <stefan.retief@bentley.com>
2021-10-07 14:42:50 -0700

    duplicate header

--- packages/modules/viewer-react/src/types.ts
+++ packages/modules/viewer-react/src/types.ts
@@ -2,10 +2,6 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-/*---------------------------------------------------------------------------------------------
- * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
- * See LICENSE.md in the project root for license terms and full copyright notice.
- *--------------------------------------------------------------------------------------------*/
 
 import { Vector3d, XAndY, XYAndZ } from "@bentley/geometry-core";
 import { IModelClient } from "@bentley/imodelhub-client";
