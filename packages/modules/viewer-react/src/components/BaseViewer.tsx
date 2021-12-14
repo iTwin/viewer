b9e068c0908c48b72d399e5afba901cdf08867bc
Arun George <aruniverse@users.noreply.github.com>
2021-12-14 17:21:27 -0500

    trying to cleanup memory leak, not sure where we could be setting state in an unmounted component here, need to look at children

--- packages/modules/viewer-react/src/components/BaseViewer.tsx
+++ packages/modules/viewer-react/src/components/BaseViewer.tsx
@@ -52,24 +52,28 @@ export const BaseViewer: React.FC<ViewerProps> = ({
     additionalRpcInterfaces,
   });
   useEffect(() => {
-    // assume authorized when using a local snapshot
-    if (snapshotPath) {
-      setAuthorized(true);
-    } else {
+    let removeIsAuthorizedListener: (() => void) | undefined = undefined;
+    const isAuthorizedListener = () => {
       setAuthorized(
         (BaseInitializer.authClient?.hasSignedIn &&
           BaseInitializer.authClient?.isAuthorized) ||
           false
       );
-      BaseInitializer.authClient?.onAccessTokenChanged.addListener(() => {
-        setAuthorized(
-          (BaseInitializer.authClient?.hasSignedIn &&
-            BaseInitializer.authClient?.isAuthorized) ||
-            false
+    };
+
+    isAuthorizedListener();
+    removeIsAuthorizedListener =
+      BaseInitializer.authClient?.onAccessTokenChanged.addListener(
+        isAuthorizedListener
+      );
+    return () => {
+      if (removeIsAuthorizedListener) {
+        BaseInitializer.authClient?.onAccessTokenChanged.removeListener(
+          removeIsAuthorizedListener
         );
-      });
-    }
-  }, [snapshotPath]);
+      }
+    };
+  }, []);
 
   return (
     <ErrorBoundary>
