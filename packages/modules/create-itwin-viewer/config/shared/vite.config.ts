223e889ac7acee158f6cca412628c481a623a005
kckst8 <6283674+kckst8@users.noreply.github.com>
2022-04-22 21:02:24 -0400

    add support for process.env in the client

--- packages/modules/create-itwin-viewer/config/shared/vite.config.ts
+++ packages/modules/create-itwin-viewer/config/shared/vite.config.ts
@@ -7,69 +7,74 @@ import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfil
 import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
 import { esbuildCommonjs, viteCommonjs } from "@kckst8/vite-plugin-commonjs";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    viteCommonjs({
-      include: [
-        "@itwin/core-frontend",
-        "@itwin/presentation-frontend",
-        "@itwin/presentation-components",
-        "@itwin/appui-react",
-      ],
-    }),
-  ],
-  resolve: {
-    alias: [
-      {
-        find: "~@itwin/itwinui-css",
-        replacement: "@itwin/itwinui-css",
-      },
-      {
-        find: "~@itwin/core-react",
-        replacement: "@itwin/core-react",
-      },
-      {
-        find: "~@itwin/appui-layout-react",
-        replacement: "@itwin/appui-layout-react",
-      },
-      {
-        find: "@itwin/itwinui-react/cjs",
-        replacement: "@itwin/itwinui-react/esm",
-      },
-    ],
-  },
-  css: {
-    preprocessorOptions: {
-      includePaths: ["node_modules"],
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          process: true,
-          buffer: true,
-        }),
-        NodeModulesPolyfillPlugin(),
-        esbuildCommonjs([
+export default defineConfig(({ mode }) => {
+  Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
+  return {
+    plugins: [
+      react(),
+      viteCommonjs({
+        include: [
           "@itwin/core-frontend",
           "@itwin/presentation-frontend",
           "@itwin/presentation-components",
           "@itwin/appui-react",
-        ]),
+        ],
+      }),
+    ],
+    resolve: {
+      alias: [
+        {
+          find: "~@itwin/itwinui-css",
+          replacement: "@itwin/itwinui-css",
+        },
+        {
+          find: "~@itwin/core-react",
+          replacement: "@itwin/core-react",
+        },
+        {
+          find: "~@itwin/appui-layout-react",
+          replacement: "@itwin/appui-layout-react",
+        },
+        {
+          find: "@itwin/itwinui-react/cjs",
+          replacement: "@itwin/itwinui-react/esm",
+        },
       ],
-      loader: {
-        ".svg": "dataurl",
-        ".woff": "dataurl",
-        ".eot": "dataurl",
-        ".ttf": "dataurl",
-        ".woff2": "dataurl",
-        ".cur": "dataurl",
+    },
+    css: {
+      preprocessorOptions: {
+        includePaths: ["node_modules"],
       },
     },
-  },
+    optimizeDeps: {
+      esbuildOptions: {
+        plugins: [
+          NodeGlobalsPolyfillPlugin({
+            process: true,
+            buffer: true,
+          }),
+          NodeModulesPolyfillPlugin(),
+          esbuildCommonjs([
+            "@itwin/core-frontend",
+            "@itwin/presentation-frontend",
+            "@itwin/presentation-components",
+            "@itwin/appui-react",
+          ]),
+        ],
+        loader: {
+          ".svg": "dataurl",
+          ".woff": "dataurl",
+          ".eot": "dataurl",
+          ".ttf": "dataurl",
+          ".woff2": "dataurl",
+          ".cur": "dataurl",
+        },
+      },
+    },
+    define: {
+      "process.env": process.env,
+    },
+  };
 });
