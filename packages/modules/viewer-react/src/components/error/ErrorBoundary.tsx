b6ff48eb438acaa2bf59a991705bb0e8f0815e0d
lecktang <leck.tang@bentley.com>
2022-05-31 15:08:42 -0500

    Remove unnecessary parts

--- packages/modules/viewer-react/src/components/error/ErrorBoundary.tsx
+++ packages/modules/viewer-react/src/components/error/ErrorBoundary.tsx
@@ -3,33 +3,23 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
-import type { ErrorPageProps, ErrorPageType } from "@itwin/itwinui-react";
 import { ErrorPage } from "@itwin/itwinui-react";
 import React, { Component } from "react";
 
 interface Props {
-  eventTitle?: string;
+  children: React.ReactNode;
 }
 
-interface Error {
-  code?: string;
-  message?: string;
+interface State {
+  fallback: boolean;
+  error: Error;
 }
 
-export class ErrorBoundary extends Component<
-  Props,
-  {
-    fallback: boolean;
-    error: Error;
-  }
-> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      fallback: false,
-      error: {},
-    };
-  }
+export class ErrorBoundary extends Component<Props, State> {
+  override state = {
+    fallback: false,
+    error: new Error(),
+  };
 
   static getDerivedStateFromError(e: Error): {
     fallback: boolean;
@@ -43,19 +33,12 @@ export class ErrorBoundary extends Component<
 
   override render(): JSX.Element {
     if (this.state.fallback) {
-      const errorType: ErrorPageType = "generic";
-      let errorMessage: ErrorPageProps["errorMessage"] = (
-        <>
-          We can't find the iModel that you are looking for or it does not
-          exist. Visit the iModelHub or contact our support team.
-        </>
+      return (
+        <ErrorPage
+          errorType="generic"
+          errorMessage={this.state.error.message}
+        />
       );
-
-      if (this.state.error.message) {
-        errorMessage = this.state.error.message;
-      }
-
-      return <ErrorPage errorType={errorType} errorMessage={errorMessage} />;
     } else {
       return <>{this.props.children}</>;
     }
