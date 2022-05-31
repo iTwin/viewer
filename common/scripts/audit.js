ac57c52a3bdeedef6dc184a68661fd6d349601d8
johnnyd710 <19596966+johnnyd710@users.noreply.github.com>
2022-05-31 17:27:40 -0400

    add cve to excludedAdvisories

--- common/scripts/audit.js
+++ common/scripts/audit.js
@@ -66,6 +66,7 @@ const rushCommonDir = path.join(__dirname, "../");
     "GHSA-phwq-j96m-2c2q", // https://github.com/advisories/GHSA-phwq-j96m-2c2q // @bentley/react-scripts>workbox-webpack-plugin>workbox-build>@surma/rollup-plugin-off-main-thread>ejs
     "GHSA-93q8-gq69-wqmw", // https://github.com/advisories/GHSA-93q8-gq69-wqmw // @itwin/build-tools>mocha>wide-align>string-width>strip-ansi>ansi-regex
     "GHSA-fwr7-v2mv-hh25", // https://github.com/advisories/GHSA-fwr7-v2mv-hh25 // @bentley/react-scripts>fast-sass-loader>async
+    "GHSA-rp65-9cf3-cjxr", // https://github.com/advisories/GHSA-rp65-9cf3-cjxr // @bentley/react-scripts>optimize-css-assets-webpack-plugin>cssnano>cssnano-preset-default>postcss-svgo>svgo>css-select>nth-check
   ];
 
   let shouldFailBuild = false;
