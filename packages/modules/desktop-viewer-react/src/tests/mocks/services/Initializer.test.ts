80d69d69ae1e69caaf48ec4b31b58184e3f2aeae
Samejima <samejimasama@gmail.com>
2022-05-31 10:51:48 -0400

    test files for the desktop viewer added

--- packages/modules/desktop-viewer-react/src/tests/mocks/services/Initializer.test.ts
+++ packages/modules/desktop-viewer-react/src/tests/mocks/services/Initializer.test.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
+
 /*---------------------------------------------------------------------------------------------
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
@@ -20,3 +21,33 @@ import type { ViewerInitializerParams } from "@itwin/viewer-react";
 
 import { DesktopInitializer } from "../../services/Initializer";
 import MockAuthorizationClient from "../mocks/MockAuthorizationClient";
+
+describe("Initializer", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    if (UiCore.initialized) {
+      UiCore.terminate();
+    }
+  });
+
+  it("initializes iModelApp", async () => {
+    await DesktopInitializer.startWebViewer({
+      authClient: new MockAuthorizationClient(),
+      enablePerformanceMonitors: false,
+    });
+    await DesktopInitializer.initialized;
+    expect(IModelApp.startup).toHaveBeenCalled();
+  });
+
+  it("Should throw an error when IModelApp.startup is called prior to rendering the Viewer", async () => {
+    await IModelApp.startup();
+    await DesktopInitializer.startWebViewer({
+      authClient: new MockAuthorizationClient(),
+      enablePerformanceMonitors: false,
+    });
+    await DesktopInitializer.initialized;
+    expect(DesktopInitializer.startWebViewer).toThrowError(
+      "Looks like you called IModelApp.startup in your application. Please use the useDesktopViewerInitializer hook instead."
+    );
+  });
+});
