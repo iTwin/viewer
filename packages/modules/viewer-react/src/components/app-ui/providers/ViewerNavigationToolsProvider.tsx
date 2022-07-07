3a09ecd5cec5a13cb63942521f1e0b1f76e0673b
Leck Tang <51834103+achrysaetos@users.noreply.github.com>
2022-07-07 16:02:29 -0500

    Use LookAndMove tool as default walk tool (#198)
    * Use LookAndMove tool as default walk tool
    
    Co-authored-by: Arun George <11051042+aruniverse@users.noreply.github.com>

--- packages/modules/viewer-react/src/components/app-ui/providers/ViewerNavigationToolsProvider.tsx
+++ packages/modules/viewer-react/src/components/app-ui/providers/ViewerNavigationToolsProvider.tsx
@@ -3,11 +3,16 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
+import type { CommonToolbarItem } from "@itwin/appui-abstract";
+import { ToolbarOrientation, ToolbarUsage } from "@itwin/appui-abstract";
 import type { DefaultNavigationTools } from "@itwin/appui-react";
+import { CoreTools } from "@itwin/appui-react";
+import { ToolbarHelper, ToolItemDef } from "@itwin/appui-react";
 import { StandardNavigationToolsProvider } from "@itwin/appui-react";
+import { IModelApp } from "@itwin/core-frontend";
 
 export class ViewerNavigationToolsProvider extends StandardNavigationToolsProvider {
-  constructor(defaultItems?: DefaultNavigationTools) {
+  constructor(private defaultItems?: DefaultNavigationTools) {
     super("ViewerDefaultNavigationTools", {
       horizontal: {
         fitView: true,
@@ -23,5 +28,124 @@ export class ViewerNavigationToolsProvider extends StandardNavigationToolsProvid
         ...defaultItems?.vertical,
       },
     });
+  }
+
+  public override provideToolbarButtonItems(
+    stageId: string,
+    stageUsage: string,
+    toolbarUsage: ToolbarUsage,
+    toolbarOrientation: ToolbarOrientation
+  ): CommonToolbarItem[] {
+    const items: CommonToolbarItem[] = [];
+
+    if (
+      toolbarUsage === ToolbarUsage.ViewNavigation &&
+      toolbarOrientation === ToolbarOrientation.Horizontal
+    ) {
+      if (
+        !this.defaultItems ||
+        !this.defaultItems.horizontal ||
+        this.defaultItems.horizontal.rotateView
+      ) {
+        items.push(
+          ToolbarHelper.createToolbarItemFromItemDef(
+            10,
+            CoreTools.rotateViewCommand
+          )
+        );
+      }
+      if (
+        !this.defaultItems ||
+        !this.defaultItems.horizontal ||
+        this.defaultItems.horizontal.panView
+      ) {
+        items.push(
+          ToolbarHelper.createToolbarItemFromItemDef(
+            20,
+            CoreTools.panViewCommand
+          )
+        );
+      }
+      if (
+        !this.defaultItems ||
+        !this.defaultItems.horizontal ||
+        this.defaultItems.horizontal.fitView
+      ) {
+        items.push(
+          ToolbarHelper.createToolbarItemFromItemDef(
+            30,
+            CoreTools.fitViewCommand
+          )
+        );
+      }
+      if (
+        !this.defaultItems ||
+        !this.defaultItems.horizontal ||
+        this.defaultItems.horizontal.windowArea
+      ) {
+        items.push(
+          ToolbarHelper.createToolbarItemFromItemDef(
+            40,
+            CoreTools.windowAreaCommand
+          )
+        );
+      }
+      if (
+        !this.defaultItems ||
+        !this.defaultItems.horizontal ||
+        this.defaultItems.horizontal.viewUndoRedo
+      ) {
+        items.push(
+          ToolbarHelper.createToolbarItemFromItemDef(
+            50,
+            CoreTools.viewUndoCommand
+          )
+        );
+        items.push(
+          ToolbarHelper.createToolbarItemFromItemDef(
+            60,
+            CoreTools.viewRedoCommand
+          )
+        );
+      }
+    } else if (
+      toolbarUsage === ToolbarUsage.ViewNavigation &&
+      toolbarOrientation === ToolbarOrientation.Vertical
+    ) {
+      if (
+        !this.defaultItems ||
+        !this.defaultItems.vertical ||
+        this.defaultItems.vertical.walk
+      ) {
+        items.push(
+          ToolbarHelper.createToolbarItemFromItemDef(
+            10,
+            new ToolItemDef({
+              toolId: "View.LookAndMove",
+              iconSpec: "icon-walk",
+              execute: () =>
+                IModelApp.tools.run(
+                  "View.LookAndMove",
+                  IModelApp.viewManager.selectedView
+                ),
+              labelKey: "iTwinViewer:tools.walkTool",
+            })
+          )
+        );
+      }
+      if (
+        !this.defaultItems ||
+        !this.defaultItems.vertical ||
+        this.defaultItems.vertical.toggleCamera
+      ) {
+        items.push(
+          ToolbarHelper.createToolbarItemFromItemDef(
+            20,
+            CoreTools.toggleCameraViewCommand
+          )
+        );
+      }
+    }
+    return items;
   }
 }
