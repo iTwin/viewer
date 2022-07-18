598803e7f8b4a6e01929b081bf13a21b0d9a3522
Leck Tang <achrysaetos@yahoo.com>
2022-07-18 16:06:04 -0500

    Add strict mode

--- packages/apps/web-viewer-test/src/index.tsx
+++ packages/apps/web-viewer-test/src/index.tsx
@@ -5,7 +5,7 @@
 
 import "./index.scss";
 
-import React from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
@@ -13,7 +13,11 @@ import * as serviceWorker from "./serviceWorker";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
