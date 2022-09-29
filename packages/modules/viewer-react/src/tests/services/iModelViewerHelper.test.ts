46d8844d8a6c87ea93d9b0b48f531495f27c90d1
Ben Polinsky <ben-polinsky@users.noreply.github.com>
2022-09-29 15:24:03 -0400

    eslint foils my hoist workaround

--- packages/modules/viewer-react/src/tests/services/iModelViewerHelper.test.ts
+++ packages/modules/viewer-react/src/tests/services/iModelViewerHelper.test.ts
@@ -5,6 +5,7 @@
 
 import { Cartographic } from "@itwin/core-common";
 import type { BlankConnectionProps } from "@itwin/core-frontend";
+import { BlankConnection } from "@itwin/core-frontend";
 import { Range3d } from "@itwin/core-geometry";
 
 import * as iModelService from "../../services/iModel/IModelService";
@@ -15,10 +16,79 @@ import {
 } from "../../services/iModel/iModelViewerHelper";
 import type { RequiredViewerConnectionProps } from "../../types";
 import { ConnectionType } from "../../types";
-import { MockCoreFrontend } from "../mocks/MockCoreFrontend";
 
 jest.mock("@itwin/core-frontend", () => {
-  return MockCoreFrontend;
+  return {
+    IModelApp: {
+      startup: jest.fn(),
+      telemetry: {
+        addClient: jest.fn(),
+      },
+      localization: {
+        getLocalizedString: jest.fn(),
+        registerNamespace: jest.fn().mockResolvedValue(true),
+      },
+      uiAdmin: {
+        updateFeatureFlags: jest.fn(),
+      },
+      notifications: {
+        openMessageBox: jest.fn(),
+      },
+      viewManager: {
+        onSelectionSetChanged: jest.fn(),
+        onViewOpen: {
+          addOnce: jest.fn(),
+        },
+      },
+    },
+    SnapMode: {},
+    ActivityMessageDetails: jest.fn(),
+    PrimitiveTool: jest.fn(),
+    NotificationManager: jest.fn(),
+    Tool: jest.fn(),
+    RemoteBriefcaseConnection: {
+      open: jest.fn(),
+    },
+    SnapshotConnection: {
+      openFile: jest.fn(),
+    },
+    MessageBoxType: {
+      Ok: 1,
+    },
+    MessageBoxIconType: {
+      Critical: 1,
+    },
+    BlankConnection: {
+      create: jest.fn().mockImplementation((params) => ({
+        isBlankConnection: () => true,
+        isOpen: true,
+        ...params,
+      })),
+    },
+    ItemField: {},
+    CompassMode: {},
+    RotationMode: {},
+    AccuDraw: class {},
+    AccuSnap: class {},
+    ToolAdmin: class {},
+    WebViewerApp: {
+      startup: jest.fn().mockResolvedValue(true),
+    },
+    ViewCreator3d: jest.fn().mockImplementation(() => {
+      return {
+        createDefaultView: jest.fn().mockResolvedValue({}),
+      };
+    }),
+    SpatialViewState: {
+      className: "",
+    },
+    DrawingViewState: {
+      className: "",
+    },
+    SheetViewState: {
+      className: "",
+    },
+  };
 });
 
 describe("iModelViewerHelper", () => {
@@ -151,6 +221,6 @@ describe("iModelViewerHelper", () => {
       iModelId: "iModelId",
     });
 
-    expect(MockCoreFrontend.BlankConnection.create).toHaveBeenCalledTimes(1);
+    expect(BlankConnection.create).toHaveBeenCalledTimes(1);
   });
 });
