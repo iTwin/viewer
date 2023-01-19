1c19921beb6ffb862989f4eb69c7e91849766622
Arun George <11051042+aruniverse@users.noreply.github.com>
2023-01-19 08:49:41 -0500

    use lodash isEqual instead of custom implementation (#232)

--- packages/modules/viewer-react/src/utilities/index.ts
+++ packages/modules/viewer-react/src/utilities/index.ts
@@ -3,62 +3,14 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
+import isEqual from "lodash.isequal";
+
 import type { ViewerCommonProps, ViewerInitializerParams } from "../types";
 import { iTwinViewerInitializerParamList } from "../types";
 
+export { isEqual };
+
 export * from "./MakeCancellable";
-
-/**
- * Is the item an object?
- * @param item
- * @returns
- */
-export const isObject = (item: any) => {
-  return Object.prototype.toString.call(item) === "[object Object]";
-};
-
-/**
- * Is the item a function?
- * @param item
- * @returns
- */
-export const isFunction = (item: any) => {
-  return typeof item === "function";
-};
-
-/**
- * Are a and b equal (deep)?
- * @param a
- * @param b
- * @returns
- */
-export const isEqual = (a?: any, b?: any) => {
-  if (a === undefined && b === undefined) {
-    return true;
-  }
-  // Do not check !a or !b as value can be boolean type
-  if (a === undefined || b === undefined) {
-    return false;
-  }
-  const aKeys = Object.keys(a);
-  const bKeys = Object.keys(b);
-  if (aKeys.length !== bKeys.length) {
-    return false;
-  }
-
-  for (const prop in a) {
-    const objectCompare = isObject(a[prop]) && isObject(b[prop]);
-    const functionCompare = isFunction(a[prop]) && isFunction(b[prop]);
-    if (
-      (objectCompare && !isEqual(a[prop], b[prop])) ||
-      (functionCompare && a[prop].toString() !== b[prop].toString()) ||
-      (!objectCompare && !functionCompare && a[prop] !== b[prop])
-    ) {
-      return false;
-    }
-  }
-  return true;
-};
 
 /**
  * Pull out iTwin.js initialization options from a set of options
