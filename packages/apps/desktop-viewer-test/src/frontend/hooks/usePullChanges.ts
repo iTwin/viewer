49439b3a4c39737d9363cd1b163d2ffedede1c0b
Gytis Cepkauskas <Gytis.Cepkauskas@bentley.com>
2023-02-03 14:10:43 +0200

    Use new progress callback.

--- packages/apps/desktop-viewer-test/src/frontend/hooks/usePullChanges.ts
+++ packages/apps/desktop-viewer-test/src/frontend/hooks/usePullChanges.ts
@@ -3,32 +3,35 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
-import type { BriefcaseConnection } from "@itwin/core-frontend";
-import type { ProgressCallback } from "@itwin/core-frontend/lib/cjs/request/Request";
+import type {
+  BriefcaseConnection,
+  OnDownloadProgress,
+} from "@itwin/core-frontend";
 import { useCallback, useState } from "react";
 
 export const usePullChanges = (connection?: BriefcaseConnection) => {
   const [pullProgress, setPullProgress] = useState<number>();
 
-  const progressCallback: ProgressCallback = useCallback((progress) => {
-    const { loaded, total, percent } = progress;
+  const downloadProgressCallback: OnDownloadProgress = useCallback(
+    (progress) => {
+      const { loaded, total } = progress;
+      const percent = loaded / total;
 
-    const progressPercentage = total ? (loaded / total) * 100 : percent;
-    setPullProgress(progressPercentage);
+      setPullProgress(percent);
 
-    console.log(
-      `Pull changes progress (${loaded}/${total}) -> ${progressPercentage}%`
-    );
-  }, []);
+      console.log(`Pull changes progress (${loaded}/${total}) -> ${percent}%`);
+    },
+    []
+  );
 
   const doPullChanges = useCallback(async () => {
     await connection?.pullChanges(undefined, {
-      progressCallback,
+      downloadProgressCallback,
       progressInterval: 100,
     });
 
     setPullProgress(undefined);
-  }, [connection, progressCallback]);
+  }, [connection, downloadProgressCallback]);
 
   return { doPullChanges, pullProgress };
 };
