060500766fce745fcf36736d5b1a3bc0c174a4ea
Jake Screen <Jake.Screen@bentley.com>
2023-02-13 14:30:43 -0500

    adding new dependency to fix web-viewer-test

--- packages/apps/web-viewer-test/src/components/routing/history.ts
+++ packages/apps/web-viewer-test/src/components/routing/history.ts
@@ -3,9 +3,11 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
-import { createBrowserHistory } from "history";
+import { createBrowserHistory } from "@remix-run/router";
 
-export const history = new History();
 
-// export const history = createBrowserHistory({ window });
+// export const history = new History();
+
+export const history = createBrowserHistory({ window });
+
 
