9623754b30f60ad3c8d37ba7d6d9dccce77367ff
Ben Polinsky <ben-polinsky@users.noreply.github.com>
2023-03-23 09:31:08 -0400

    no reason to memoize a static; add more types

--- packages/templates/cra-template-web-viewer/template/src/Auth.ts
+++ packages/templates/cra-template-web-viewer/template/src/Auth.ts
@@ -2,18 +2,21 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
+
 import type { BrowserAuthorizationClientConfiguration } from "@itwin/browser-authorization";
 import { BrowserAuthorizationClient } from "@itwin/browser-authorization";
 
 export class Auth {
   private static client: BrowserAuthorizationClient;
 
-  public static initialize(options: BrowserAuthorizationClientConfiguration) {
+  public static initialize(
+    options: BrowserAuthorizationClientConfiguration
+  ): BrowserAuthorizationClient {
     this.client = new BrowserAuthorizationClient(options);
     return this.client;
   }
 
-  public static getClient() {
+  public static getClient(): BrowserAuthorizationClient {
     if (!this.client) {
       throw new Error(
         "Client not initialized. Please call `Auth.initialize(BrowserAuthorizationClientConfiguration)`"
