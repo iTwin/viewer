cc35c61d68c4b6831da34aed3af0d9c67b06e7ed
Jake-Screen <Jake-Screen@users.noreply.github.com>
2023-05-23 16:05:21 -0400

    fixing desktop template

--- packages/templates/cra-template-desktop-viewer/template/src/frontend/components/routes/HomeRoute.tsx
+++ packages/templates/cra-template-desktop-viewer/template/src/frontend/components/routes/HomeRoute.tsx
@@ -17,7 +17,7 @@ export const HomeRoute = () => {
   useEffect(() => {
     // must be initialized here (child of the Router) in order to use the navigate function
     ITwinViewerApp.initializeMenuListeners(navigate, userSettings);
-  }, []);
+  }, [navigate, userSettings]);
 
   return <Home />;
 };
