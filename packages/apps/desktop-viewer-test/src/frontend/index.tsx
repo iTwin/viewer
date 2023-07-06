6683a90185b5baa76059b01435774e1b2608dfff
Jake-Screen <Jake-Screen@users.noreply.github.com>
2023-07-06 15:15:57 -0400

    linting

--- packages/apps/desktop-viewer-test/src/frontend/index.tsx
+++ packages/apps/desktop-viewer-test/src/frontend/index.tsx
@@ -22,8 +22,8 @@ const viewerFrontendMain = async () => {
   document.documentElement.classList.add(`iui-theme-dark`);
 
   // when initialization is complete, render
-  const container = document.getElementById("root");
-  const root = createRoot(container!);
+  const container = document.getElementById("root") as HTMLElement;
+  const root = createRoot(container);
 
   root.render(
     <React.StrictMode>
