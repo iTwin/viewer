7bb0ef57785891e19ff056cf33e361e59eb1bdfc
An.Duong <An.Duong@bentley.com>
2023-09-07 14:02:18 -0700

    Remove unused imports

--- packages/modules/desktop-viewer-react/src/types.ts
+++ packages/modules/desktop-viewer-react/src/types.ts
@@ -3,7 +3,6 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
-import { MarkRequired, Optional } from "@itwin/core-bentley";
 import type { RpcInterface, RpcInterfaceDefinition } from "@itwin/core-common";
 import type {
   BlankViewerProps,
