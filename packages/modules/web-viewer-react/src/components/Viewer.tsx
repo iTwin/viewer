c27704a179ab9352e470f13be8a63f06fd379ba9
Hoang Nam Le <50554904+hl662@users.noreply.github.com>
2024-02-22 16:21:51 -0500

    add imodelapp shutdown on page refresh or navigation away

--- packages/modules/web-viewer-react/src/components/Viewer.tsx
+++ packages/modules/web-viewer-react/src/components/Viewer.tsx
@@ -3,13 +3,25 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
+import { IModelApp } from "@itwin/core-frontend";
 import { BaseViewer } from "@itwin/viewer-react";
-import React from "react";
+import React, { useEffect } from "react";
 
 import { useWebViewerInitializer } from "../hooks";
 import type { WebViewerProps } from "../types";
 
 export const Viewer = (props: WebViewerProps) => {
   const initialized = useWebViewerInitializer(props);
+  useEffect(() => {
+    const handleBeforeUnload = async () => {
+      IModelApp.shutdown()
+        .then(() => console.log("Shutdown success."))
+        .catch(() => console.warn("Shutdown failed."));
+    };
+    window.addEventListener("beforeunload", handleBeforeUnload);
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
   return initialized ? <BaseViewer {...props} /> : null;
 };
