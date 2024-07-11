e20a75bc8def6100da19cbfd1adee8341c5eaff2
Ben Polinsky <ben-polinsky@users.noreply.github.com>
2024-07-11 15:35:11 -0400

    blurgh

--- packages/modules/viewer-react/src/services/BaseInitializer.ts
+++ packages/modules/viewer-react/src/services/BaseInitializer.ts
@@ -3,7 +3,6 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
-import type { UiSyncEventArgs } from "@itwin/appui-abstract";
 import {
   AppNotificationManager,
   FrameworkAccuDraw,
