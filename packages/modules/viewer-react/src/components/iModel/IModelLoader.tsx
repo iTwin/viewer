b89b2814469bf6b9fed179d1b068a1a43479a087
Saulius Skliutas <24278440+saskliutas@users.noreply.github.com>
2024-08-22 10:56:14 +0300

    Fix possible race condition in StrictMode (#321)
    * Make useEffect symmetrical and avoid race conditions
    
    * Change

--- packages/modules/viewer-react/src/components/iModel/IModelLoader.tsx
+++ packages/modules/viewer-react/src/components/iModel/IModelLoader.tsx
@@ -16,15 +16,10 @@ import type { IModelConnection } from "@itwin/core-frontend";
 import { IModelApp } from "@itwin/core-frontend";
 import { SvgIModelLoader } from "@itwin/itwinui-illustrations-react";
 import { Presentation } from "@itwin/presentation-frontend";
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { Provider } from "react-redux";
 
-import {
-  useFrontstages,
-  useIsMounted,
-  useTheme,
-  useUiProviders,
-} from "../../hooks";
+import { useFrontstages, useTheme, useUiProviders } from "../../hooks";
 import {
   gatherRequiredViewerProps,
   getAndSetViewState,
@@ -37,9 +32,6 @@ import { IModelViewer } from "./IModelViewer";
 import { useUnifiedSelectionSync } from "../../hooks/useUnifiedSelectionSync";
 
 const IModelLoader = React.memo((viewerProps: ModelLoaderProps) => {
-  const [error, setError] = useState<Error>();
-  const [connection, setConnection] = useState<IModelConnection>();
-  const isMounted = useIsMounted();
   const {
     frontstages,
     defaultUiConfig,
@@ -48,12 +40,12 @@ const IModelLoader = React.memo((viewerProps: ModelLoaderProps) => {
     blankConnectionViewState,
     uiProviders,
     theme,
-    onIModelConnected,
     backstageItems, // eslint-disable-line deprecation/deprecation
     loadingComponent,
     selectionStorage,
     getSchemaContext,
   } = viewerProps;
+  const { error, connection } = useConnection(viewerProps);
 
   const providers = useMemo<UiItemsProvider[]>(() => {
     const providers = [...(uiProviders || [])];
@@ -77,103 +69,6 @@ const IModelLoader = React.memo((viewerProps: ModelLoaderProps) => {
     });
 
   useTheme(theme);
-
-  // This preserves how the list of selection scopes was synced between Presentation and AppUi before its removal in 4.x
-  const syncSelectionScopeList = useCallback(
-    async (iModelConnection: IModelConnection) => {
-      if (iModelConnection.isBlankConnection() || !iModelConnection.isOpen) {
-        return;
-      }
-
-      // Fetch the available selection scopes and add them to the redux store
-      try {
-        const availableScopes =
-          await Presentation.selection.scopes.getSelectionScopes(
-            iModelConnection
-          );
-        UiFramework.dispatchActionToStore(
-          SessionStateActionId.SetAvailableSelectionScopes,
-          availableScopes
-        );
-      } catch {
-        console.log("Error syncing selection scope list.");
-      }
-    },
-    []
-  );
-
-  const getModelConnection = useCallback(async (): Promise<
-    IModelConnection | undefined
-  > => {
-    const requiredConnectionProps = gatherRequiredViewerProps(viewerProps);
-
-    if (!requiredConnectionProps) {
-      throw new Error(
-        IModelApp.localization.getLocalizedString([
-          "iTwinViewer",
-          "missingConnectionProps",
-        ])
-      );
-    }
-
-    ViewerPerformance.addMark("IModelConnectionStarted");
-    ViewerPerformance.addMeasure(
-      "IModelConnecting",
-      "ViewerStarting",
-      "IModelConnectionStarted"
-    );
-
-    // create a new imodelConnection for the passed project and imodel ids or local file
-    const imodelConnection = await openConnection(requiredConnectionProps);
-
-    ViewerPerformance.addMark("IModelConnection");
-    ViewerPerformance.addMeasure(
-      "IModelConnected",
-      "ViewerStarting",
-      "IModelConnection"
-    );
-
-    if (imodelConnection && isMounted.current) {
-      // Tell the SyncUiEventDispatcher and StateManager about the iModelConnection
-      UiFramework.setIModelConnection(imodelConnection, true);
-
-      if (onIModelConnected) {
-        await onIModelConnected(imodelConnection);
-      }
-      setConnection(imodelConnection);
-
-      await syncSelectionScopeList(imodelConnection);
-
-      return imodelConnection;
-    }
-    return;
-  }, [
-    viewerProps.iTwinId,
-    viewerProps.iModelId,
-    viewerProps.changeSetId,
-    viewerProps.filePath,
-    isMounted,
-    onIModelConnected,
-  ]);
-
-  useEffect(() => {
-    let prevConnection: IModelConnection | undefined;
-
-    void getModelConnection()
-      .then((connection) => (prevConnection = connection))
-      .catch(setError);
-
-    const mounted = isMounted.current;
-    return () => {
-      if (prevConnection) {
-        void prevConnection.close();
-        prevConnection = undefined;
-      }
-      if (mounted) {
-        setConnection(undefined);
-      }
-    };
-  }, [getModelConnection, isMounted]);
 
   useEffect(() => {
     if (customDefaultFrontstage && connection) {
@@ -222,5 +117,115 @@ const IModelLoader = React.memo((viewerProps: ModelLoaderProps) => {
     );
   }
 });
+
+function useConnection(viewerProps: ModelLoaderProps) {
+  const [error, setError] = useState<Error>();
+  const [connection, setConnection] = useState<IModelConnection>();
+  const onIModelConnected = viewerProps.onIModelConnected;
+
+  useEffect(() => {
+    setConnection(undefined);
+
+    let disposed = false;
+    const connectionPromise = getConnection(viewerProps);
+    const prepareConnection = async () => {
+      try {
+        const imodelConnection = await connectionPromise;
+        if (disposed) {
+          return;
+        }
+
+        setConnection(imodelConnection);
+        if (imodelConnection) {
+          // Tell the SyncUiEventDispatcher and StateManager about the iModelConnection
+          UiFramework.setIModelConnection(imodelConnection, true);
+
+          if (onIModelConnected) {
+            await onIModelConnected(imodelConnection);
+          }
+
+          await syncSelectionScopeList(imodelConnection);
+        }
+      } catch (error: unknown) {
+        if (!disposed) {
+          setError(error as Error);
+        }
+      }
+    };
+
+    void prepareConnection();
+
+    return () => {
+      disposed = true;
+      const closeConnection = async () => {
+        const imodelConnection = await connectionPromise;
+        if (imodelConnection) {
+          await imodelConnection.close();
+        }
+      };
+      void closeConnection();
+    };
+  }, [
+    viewerProps.iTwinId,
+    viewerProps.iModelId,
+    viewerProps.changeSetId,
+    viewerProps.filePath,
+    onIModelConnected,
+  ]);
+
+  return { connection, error };
+}
+
+async function getConnection(
+  viewerProps: ModelLoaderProps
+): Promise<IModelConnection | undefined> {
+  const requiredConnectionProps = gatherRequiredViewerProps(viewerProps);
+
+  if (!requiredConnectionProps) {
+    throw new Error(
+      IModelApp.localization.getLocalizedString([
+        "iTwinViewer",
+        "missingConnectionProps",
+      ])
+    );
+  }
+
+  ViewerPerformance.addMark("IModelConnectionStarted");
+  ViewerPerformance.addMeasure(
+    "IModelConnecting",
+    "ViewerStarting",
+    "IModelConnectionStarted"
+  );
+
+  // create a new imodelConnection for the passed project and imodel ids or local file
+  const imodelConnection = await openConnection(requiredConnectionProps);
+
+  ViewerPerformance.addMark("IModelConnection");
+  ViewerPerformance.addMeasure(
+    "IModelConnected",
+    "ViewerStarting",
+    "IModelConnection"
+  );
+
+  return imodelConnection;
+}
+
+async function syncSelectionScopeList(iModelConnection: IModelConnection) {
+  if (iModelConnection.isBlankConnection() || !iModelConnection.isOpen) {
+    return;
+  }
+
+  // Fetch the available selection scopes and add them to the redux store
+  try {
+    const availableScopes =
+      await Presentation.selection.scopes.getSelectionScopes(iModelConnection);
+    UiFramework.dispatchActionToStore(
+      SessionStateActionId.SetAvailableSelectionScopes,
+      availableScopes
+    );
+  } catch {
+    console.log("Error syncing selection scope list.");
+  }
+}
 
 export default IModelLoader;
