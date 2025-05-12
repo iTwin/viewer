6ecc61324215c21da7d4a65fae7d129668af1660
Pankhur Dhaila <pankhur.dhaila@bentley.com>
2025-05-12 09:51:28 -0600

    removing tsconfig.test, updating ts

--- packages/modules/viewer-react/eslint.config.js
+++ packages/modules/viewer-react/eslint.config.js
@@ -2,12 +2,12 @@ const config = require("../../../common/scripts/eslint.config.js");
 
 module.exports = {
   ...config,
+  ignores: ["./src/**/*.test.ts*"], // Ignore test files for now, to be removed later once exclusion from tsconfig is removed.
   languageOptions: {
     ...config.languageOptions,
     parserOptions: {
       ...config.languageOptions.parserOptions,
       tsconfigRootDir: __dirname,
-      project: ["./tsconfig.json", "./tsconfig.test.json"],
     },
   },
 };
