35ec69043aa43e5cd6cb18f206e2946d165e374b
Pankhur Dhaila <pankhur.dhaila@bentley.com>
2025-07-14 12:03:50 -0600

    add env variables check for runtime, avoid failure during compilation

--- packages/templates/web/src/index.tsx
+++ packages/templates/web/src/index.tsx
@@ -10,6 +10,22 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
 import { router } from "./components/Routes";
 
+if (!import.meta.env.IMJS_AUTH_CLIENT_CLIENT_ID) {
+  throw new Error(
+    "Please add a valid OIDC client id to the .env file and restart. See the README for more information."
+  );
+}
+if (!import.meta.env.IMJS_AUTH_CLIENT_SCOPES) {
+  throw new Error(
+    "Please add valid scopes for your OIDC client to the .env file and restart. See the README for more information."
+  );
+}
+if (!import.meta.env.IMJS_AUTH_CLIENT_REDIRECT_URI) {
+  throw new Error(
+    "Please add a valid redirect URI to the .env file and restart. See the README for more information."
+  );
+}
+
 declare module "@tanstack/react-router" {
   interface Register {
     router: typeof router;
--- packages/templates/web/vite.config.ts
+++ packages/templates/web/vite.config.ts
@@ -1,29 +1,11 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 import react from "@vitejs/plugin-react";
 
 const ENV_PREFIX = "IMJS_";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), ENV_PREFIX);
-
-  if (!env.IMJS_AUTH_CLIENT_CLIENT_ID) {
-    throw new Error(
-      "Please add a valid OIDC client id to the .env file and restart. See the README for more information."
-    );
-  }
-  if (!env.IMJS_AUTH_CLIENT_SCOPES) {
-    throw new Error(
-      "Please add valid scopes for your OIDC client to the .env file and restart. See the README for more information."
-    );
-  }
-  if (!env.IMJS_AUTH_CLIENT_REDIRECT_URI) {
-    throw new Error(
-      "Please add a valid redirect URI to the .env file and restart. See the README for more information."
-    );
-  }
-
+export default defineConfig(() => {
   return {
     plugins: [
       react(),
